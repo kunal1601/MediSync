54505cd72e6326180794c7ba08d3d28c3f2ced1b
VivekHBanthiya <vivekbanthiya31@gmail.com>
2026-07-28 19:05:12 +0530

    fix: restore SalesInvoiceItem entity

---
+++ Project/medisync-backend/src/main/java/com/medisync/medisync_backend/entity/SalesInvoiceItem.java
@@ -0,0 +1,49 @@
+package com.medisync.medisync_backend.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="sales_invoice_items")
+@Getter
+@Setter
+public class SalesInvoiceItem {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="invoice_item_id")
+	private Integer invoiceItemId;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="invoice_id",nullable=false)
+	private SalesInvoice salesInvoice;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="medicine_id" , nullable=false)
+	private Medicine medicine;
+	 
+    @ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "stock_id")
+	private MedicineStock stock;
+    
+    
+	@Column(nullable=false)
+	private Integer quantity;
+	
+	@Column(nullable=false,precision=10,scale=2)
+	private BigDecimal  price;
+	
+	public SalesInvoiceItem() {
+    }
+}
