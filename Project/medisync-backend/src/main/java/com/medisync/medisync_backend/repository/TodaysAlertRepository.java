cb75d2727eb5ec902ed55e452b5358129313d869
Kunal <kunalsharma1601@gmail.com>
2026-08-09 14:07:39 +0530

    alert mechanism changes

--- Project/medisync-backend/src/main/java/com/medisync/medisync_backend/repository/TodaysAlertRepository.java
+++ Project/medisync-backend/src/main/java/com/medisync/medisync_backend/repository/TodaysAlertRepository.java
@@ -4,38 +4,68 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.medisync.medisync_backend.entity.MedicineStock;
 
+@Repository
 public interface TodaysAlertRepository extends JpaRepository<MedicineStock, Long> {
-	 /*
-     * Medicines having zero stock
+
+    /*
+     * Medicine is Out Of Stock only when
+     * total stock across all its batches is 0.
      */
 	@Query("""
-			SELECT ms 
-			FROM MedicineStock ms
-			WHERE ms.stockQuantity=0
-			""")
-	List<MedicineStock> getOutOfStockMedicines();
-	
-	 /*
-     * Medicines expiring within next 30 days
+		    SELECT ms
+		    FROM MedicineStock ms
+		    WHERE ms.medicine.id IN (
+		        SELECT ms2.medicine.id
+		        FROM MedicineStock ms2
+		        GROUP BY ms2.medicine.id
+		        HAVING SUM(ms2.stockQuantity) = 0
+		    )
+		    AND ms.id = (
+		        SELECT MIN(ms3.id)
+		        FROM MedicineStock ms3
+		        WHERE ms3.medicine.id = ms.medicine.id
+		    )
+		""")
+		List<MedicineStock> getOutOfStockMedicines();
+
+
+    /*
+     * Medicine is Near Expiry if any batch
+     * expires within the next 30 days.
      */
 	@Query(value = """
-		    SELECT *
-		    FROM medicine_stock
-		    WHERE expiry_date BETWEEN CURDATE()
+		    SELECT ms.*
+		    FROM medicine_stock ms
+		    WHERE ms.expiry_date BETWEEN CURDATE()
 		    AND DATE_ADD(CURDATE(), INTERVAL 30 DAY)
-		    """, nativeQuery = true)
-	List<MedicineStock> getNearExpiryMedicines();
-	
-	   /*
-     * Medicines already expired
+		    AND ms.stock_id = (
+		        SELECT MIN(ms2.stock_id)
+		        FROM medicine_stock ms2
+		        WHERE ms2.medicine_id = ms.medicine_id
+		        AND ms2.expiry_date BETWEEN CURDATE()
+		        AND DATE_ADD(CURDATE(), INTERVAL 30 DAY)
+		    )
+		""", nativeQuery = true)
+		List<MedicineStock> getNearExpiryMedicines();
+
+
+    /*
+     * Medicine is Expired if any batch has expired.
      */
 	@Query("""
-			SELECT ms
-			FROM MedicineStock ms
-			WHERE ms.expiryDate<CURRENT_DATE
-			""")
-	List<MedicineStock> getExpiredMedicines();
+		    SELECT ms
+		    FROM MedicineStock ms
+		    WHERE ms.expiryDate < CURRENT_DATE
+		    AND ms.id = (
+		        SELECT MIN(ms2.id)
+		        FROM MedicineStock ms2
+		        WHERE ms2.medicine.id = ms.medicine.id
+		        AND ms2.expiryDate < CURRENT_DATE
+		    )
+		""")
+		List<MedicineStock> getExpiredMedicines();
 }
